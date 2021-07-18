446f7e8826bdc40a3d0969fc8bb2b59167f79f14
Nick Coppola <ncoppol@gmail.com>
2021-07-18 18:17:42 +0000

    created just one table (url) to hold shorturl, fullurl, and number of clicks --- we use primary key (id) of this table to determine shorturl --- setting id to type int, allows us to create over 4 billion unique short url's, can change to bigint later if we need more --- setting fullurl to varchar(10000), default max uri for apache and nginx is around 8000 bytes

---
+++ app/Database/Migrations/2021-07-18-171024_InitializeDB.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class InitializeDB extends Migration
+{
+	public function up()
+	{
+		$fields = [
+			'short' => [
+				'type' => 'VARCHAR',
+				'constraint' => '10',
+				'null' => true
+			],
+			'full' => [
+				'type' => 'VARCHAR',
+				'constraint' => '10000',
+				'null' => true
+			],
+			'clicks' => [
+				'type' => 'INT',
+				'constraint' => '11',
+				'default' => '0'
+			]
+		];
+		$this->forge->addField('id');
+		$this->forge->addField($fields);
+		$this->forge->createTable('urls');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('urls');
+	}
+}
